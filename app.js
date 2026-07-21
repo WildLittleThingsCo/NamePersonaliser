f098e8ef0d073dc0b5d3253d34e8d833b889c73b
WildLittleThingsCo <rhysenorthey@hotmail.com>
2026-07-21 13:47:50 +1000

    Update app.js

--- app.js
+++ app.js
@@ -62,7 +62,7 @@ let mochiFontLoaded = false;
 // Load Mochi Boom for both the controls and canvas.
 const mochiFont = new FontFace(
   "Mochi",
-  'url("./MochiBoom.ttf")'
+  'url("./Mochi Boom.otf")'
 );
 
 async function loadMochiFont() {
