e4d220a744aecab1126a8724a53c6e9724c2a7d0
WildLittleThingsCo <rhysenorthey@hotmail.com>
2026-08-19 23:24:03 +1000

    Adjust highlight position calculations in app.js

--- app.js
+++ app.js
@@ -739,8 +739,8 @@ function drawGlossyFinish(
 
   // 3. Main ceramic highlight
   // Small curved-looking white streak near upper-left
-  const highlightX = x + fontSize * 0.14;
-  const highlightY = y - fontSize * 0.24;
+  const highlightX = x + fontSize * 0.065;
+  const highlightY = y - fontSize * 0.27;
 
   ctx.globalAlpha = 0.82;
   ctx.strokeStyle = "#ffffff";
